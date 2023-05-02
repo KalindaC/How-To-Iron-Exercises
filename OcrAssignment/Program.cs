623d1aebe35aa5c658d8ab69e030ee1657a6238a
KalindaC <32706150+KalindaC@users.noreply.github.com>
2023-05-02 16:19:28 +0700

    Add OcrAssignment

---
+++ OcrAssignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using IronOcr;
+
+var ocr = new IronTesseract();
+
+using(var input = new OcrInput())
+{
+    input.AddImage("C:/Users/kalin/source/repos/IronicAssignments/OcrAssignment/Assets/Images/img_5037.jpg");
+    //input.AddPdf("book_page.pdf");
+
+    OcrResult result = ocr.Read(input);
+    string text = result.Text;
+    result.SaveAsSearchablePdf("book_page.pdf")
+}
